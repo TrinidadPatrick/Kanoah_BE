5bbebc48912ae4fc9bec2def09bfe5b011103d3e
John Patrick Trinidad <Ptrinidad765@gmail.com>
2023-12-18 22:58:57 +0800

    s

--- socket.js
+++ socket.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5001", "http://localhost:3000"],
+        origin: ["http://localhost:5001", "http://localhost:3000", 'https://web-based-service-finder.vercel.app'],
         methods: ['GET', 'POST'],
         
     }
