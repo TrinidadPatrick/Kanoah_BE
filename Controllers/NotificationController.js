7d646bb48060e8448e3fb6a862bb9e4c88d7cdb3
John Patrick Trinidad <Ptrinidad765@gmail.com>
2024-04-02 00:30:22 +0800

    updated

--- Controllers/NotificationController.js
+++ Controllers/NotificationController.js
@@ -139,7 +139,7 @@ module.exports.markAllAsRead = async (req,res) => {
   const markAllAsRead = async (userId) => {
     try {
       const result = await notifications.updateMany({notif_to : userId, isRead : false}, {$set : {isRead : true}})
-      return res.status(200)
+      return res.status(200).json({message : "success"})
     } catch (error) {
       return res.status(500).json(error)
     }
