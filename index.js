f63cfb844e3a51d9d2bf198c03902fe56276a820
John Patrick Trinidad <Ptrinidad765@gmail.com>
2024-08-26 12:42:47 +0800

    updated

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const app = express()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5000", "http://localhost:3001", "http://localhost:3000", "http://localhost:3001", 'https://web-based-service-finder.vercel.app', 'https://kanoah.onrender.com', 'https://kanoah-web.vercel.app', 'https://kanoah-be.vercel.app'],
+        origin: ["http://localhost:5000",, "http://localhost:3000",, 'https://web-based-service-finder.vercel.app', 'https://kanoah.onrender.com', 'https://kanoah-web.vercel.app', 'https://kanoah-be.vercel.app'],
         methods: ['GET', 'POST'],
         credentials: true,
     },
 });
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'http://localhost:3001', "http://localhost:3001",  'https://web-based-service-finder.vercel.app', 'https://kanoah.onrender.com', 'exp://192.168.55.106:8081', 'http://192.168.55.106:8081/_expo/loading', 'https://kanoah-web.vercel.app', 'https://kanoah-be.vercel.app/']
+    origin: ['http://localhost:3000',  'https://web-based-service-finder.vercel.app', 'https://kanoah.onrender.com', 'exp://192.168.55.106:8081', 'http://192.168.55.106:8081/_expo/loading', 'https://kanoah-web.vercel.app', 'https://kanoah-be.vercel.app/']
 }))
 app.use(cookieParser())
 app.use(express.json())
